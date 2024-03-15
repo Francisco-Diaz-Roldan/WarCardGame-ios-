550a2f9faf7dbab824ba3099a2d7882bf10de535
Francisco Diaz Roldan <francisco.diazro@viewnet.com>
2024-03-15 06:29:55 -0700

    Initial Commit

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Francisco Díaz Roldán on 15/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
